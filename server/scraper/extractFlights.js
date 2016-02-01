82003af0120fc2111373eff1ea0e9f2828609641
logason <logason@gmail.com>
2016-02-01 23:26:41 +0000

    Detect when flights are delayed over midnight

--- server/scraper/extractFlights.js
+++ server/scraper/extractFlights.js
@@ -5,7 +5,10 @@ const cheerio = require('cheerio');
 module.exports = (data, year) => {
   const $ = cheerio.load(data);
   const columns = ['date', 'flightNum', 'airline', 'location', 'scheduled', 'status'];
-  const flights = [];
+  let flights = [];
+
+  let lastFlightTime = 0;
+  let foundYesterdayFlights = false;
 
   $('tr').map((rowNum, flightRow) => {
     if (!$(flightRow).children('td').length) {
@@ -28,8 +31,22 @@ module.exports = (data, year) => {
     });
 
     flight.timestamp = new Date(`${year} ${flight.date} ${flight.scheduled}`).getTime();
-    flight.id = flight.timestamp + flight.flightNum;
 
+    // Check if we can find flights that were delayed over midnight
+    const thisFlightTime = parseInt(flight.scheduled.split(':')[0], 10);
+    if (!foundYesterdayFlights && lastFlightTime - thisFlightTime > 10) {
+      flights = flights.map((item) => {
+        // All flights in flights[] were delayed over midnight.
+        // Therefor we need to correct their timestamp.
+        item.timestamp = item.timestamp - (24 * 60 * 60 * 1000);
+        item.id = item.timestamp + item.flightNum;
+        return item;
+      });
+      foundYesterdayFlights = true;
+    }
+    lastFlightTime = thisFlightTime;
+
+    flight.id = flight.timestamp + flight.flightNum;
     flights.push(flight);
   });
 
